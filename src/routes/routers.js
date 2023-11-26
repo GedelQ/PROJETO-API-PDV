7da3c09f156ad833b3f04e2c2c59ecc74a17fd80
Olivino <olivino.hm@gmail.com>
2023-11-26 12:29:45 -0300

    criação de configuração do endpoit de cadastrar usuário

--- src/routes/routers.js
+++ src/routes/routers.js
@@ -2,9 +2,12 @@
 // const schemaUser = require('./schemas/schemaUser')
 const express = require("express")
 const { listCategories } = require("../controllers/categorieController")
+const { userRegister } = require("../controllers/userController")
 
 const routes = express()
 
 routes.get("/categoria", listCategories)
+
+routes.post("/usuario", userRegister)
 
 module.exports = routes
