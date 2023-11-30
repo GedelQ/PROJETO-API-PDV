abdf1225c23ef071649a805dd0be50501452d62c
Olivino <olivino.hm@gmail.com>
2023-11-30 14:36:21 -0300

    criação do endpoint de listar produtos

--- src/routes/routers.js
+++ src/routes/routers.js
@@ -1,26 +1,27 @@
-const express = require("express")
-const { listCategories } = require("../controllers/categorieController")
-const { login } = require("../controllers/loginContoller")
-const { userRegister, userDetail, userUpdate } = require("../controllers/userController")
-const checkUserToken = require("../middlewares/middlewares")
-const validateRequestBody = require("../middlewares/validateRequestBody")
-const schemaLogin = require("../schemas/schemaLogin")
-const schemaUser = require("../schemas/schemaUser")
-const { listProducts } = require("../controllers/productController")
-const routes = express()
+const express = require("express");
+const { listCategories } = require("../controllers/categorieController");
+const { login } = require("../controllers/loginContoller");
+const {
+  userRegister,
+  userDetail,
+  userUpdate,
+} = require("../controllers/userController");
+const checkUserToken = require("../middlewares/middlewares");
+const validateRequestBody = require("../middlewares/validateRequestBody");
+const schemaLogin = require("../schemas/schemaLogin");
+const schemaUser = require("../schemas/schemaUser");
+const { listProducts } = require("../controllers/productController");
+const { listCustomers } = require("../controllers/customerController");
+const routes = express();
 
+routes.get("/categoria", listCategories);
+routes.post("/usuario", validateRequestBody(schemaUser), userRegister);
+routes.post("/login", validateRequestBody(schemaLogin), login);
+routes.use(checkUserToken);
+routes.get("/usuario", userDetail);
+routes.put("/usuario", validateRequestBody(schemaUser), userUpdate);
+routes.get("/produto", listProducts);
 
-routes.get("/categoria", listCategories)
-routes.post("/usuario", validateRequestBody(schemaUser), userRegister)
-routes.post("/login", validateRequestBody(schemaLogin), login)
-routes.use(checkUserToken)
-routes.get("/usuario", userDetail)
-routes.put("/usuario", validateRequestBody(schemaUser), userUpdate)
-routes.get("/produto", listProducts)
+routes.get("/cliente", listCustomers);
 
-
-module.exports = routes
-
-
-
-
+module.exports = routes;
