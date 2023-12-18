43b639ba1bc670c9169fe9a5095324ac21ab468b
Miti <mitisuaki@gmail.com>
2023-12-18 10:08:51 -0300

    fixed a problem when inputing only spaces

--- src/middlewares/validadeIDs.js
+++ src/middlewares/validadeIDs.js
@@ -2,11 +2,15 @@ const { schemaCaterogiaIDArray, schemaClienteIDArray } = require("../schemas/sch
 
 const validateCategoriaID = (joiSchema) => async (req, res, next) => {
     try {
-        req.query.categoria_id = req.query.categoria_id.trim()
-        if (typeof req.query.categoria_id === typeof "String" && req.query.categoria_id !== "") {
-            req.query.categoria_id = +req.query.categoria_id
-            await joiSchema.validateAsync(req.query)
-        } else if (req.query.categoria_id !== "") {
+        const type = typeof req.query.categoria_id
+
+        if (type === typeof "String") {
+            req.query.categoria_id = req.query.categoria_id.trim()
+            if (req.query.categoria_id !== "") {
+                req.query.categoria_id = +req.query.categoria_id
+                await joiSchema.validateAsync(req.query)
+            }
+        } else if (type === typeof []) {
             req.query.categoria_id = req.query.categoria_id.map(n => n = +n)
             joiSchema = schemaCaterogiaIDArray;
             await joiSchema.validateAsync(req.query)
